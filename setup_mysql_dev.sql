f913c3ecbb725a4c974ad32223516f0a0fcda657
ramaanoFullStack <rsajcm.m@icloud.com>
2023-03-22 12:34:24 -0700

    a script that prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,12 @@
---Script sets up a new database for the project, creates a new user with all privileges on the database, and grants an additional SELECT privilege on another database.
---Creates a database called "hbnb_dev_db" if it doesn't already exist.
+-- this script prepares a MySQL server for the project
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---Creates a user named "hbnb_dev" with the password "hbnb_dev_pwd" if it doesn't already exist.
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---Grants all privileges on the "hbnb_dev_db" database to the "hbnb_dev" user.
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---Flushes the privileges to ensure that the changes take effect immediately.
 FLUSH PRIVILEGES;
---Grants the SELECT privilege for the "hbnb_dev" user in the "performance_schema" database.
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
---Flushes the privileges again to ensure that the new privilege is applied.
 FLUSH PRIVILEGES;
